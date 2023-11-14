997a4376501ff7260eabc6e8742da2567c3e64dc
Pawan Nirpal <pawannirpal@gmail.com>
2023-11-14 19:52:18 +0530

    parallel pi computation added

--- HPC/computing_pi_using_numerical_integration.cpp
+++ HPC/computing_pi_using_numerical_integration.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <omp.h>
+#include <chrono>
 using namespace std;
 
 /*
@@ -20,7 +21,36 @@ void computePiSerial(){
     cout<<pi;
 }
 
+void computePiParallel(){
+    const int MAX_NUM_T = 100;
+    int numThreads;
+    double pi = 0.0;
+    double sum[MAX_NUM_T];
+    deltaX = 1.0/(double(numSteps));
+    #pragma omp parallel
+    {
+        double x;
+        int id = omp_get_thread_num();
+        int nThreads = omp_get_num_threads();
+        // let only one thread record this thread count. To prevent all threads writing to it. 
+        if(id==0)
+            numThreads = nThreads;
+        for(int i = id;i<numSteps;i+=nThreads){
+            x = (i+0.5)*deltaX;
+            double fx = (4.0/(1.0+x*x))*deltaX;
+            sum[id]+=fx;
+        }
+    }
+    for(int i = 0;i<numThreads;i++)
+        pi+=sum[i];
+    cout<<pi;
+}
+
 int main(){
-    computePiSerial();
+    auto start_time = std::chrono::high_resolution_clock::now();
+    computePiParallel();
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+    std::cout << "Time taken by function f: " << duration.count() << " nanoseconds" << std::endl;
     return 0;
 }
